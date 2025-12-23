6b43615a2dab7b9c2651a12eba518551878c3f36
bala-amypo <bala@amypo.in>
2025-12-23 04:02:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,24 +1,24 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.example.demo.entity.User;
+import com.example.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService service;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return service.register(user);
+    }
+
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody Map<String, String> request) {
-
-        String username = request.get("username");
-        String password = request.get("password");
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("username", username);
-        response.put("status", "LOGIN_SUCCESS");
-
-        return response;
+    public User login(@RequestBody User user) {
+        return service.login(user);
     }
 }
